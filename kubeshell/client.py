e2a1c0a3513b473a9ab8ba0754eca9dcad8cd44b
Josef-Ondrej <josef.ondrej@ibm.com>
2019-06-03 11:25:24 +0200

    Add namespace resource calls with caching

--- kubeshell/client.py
+++ kubeshell/client.py
@@ -2,7 +2,9 @@ from __future__ import absolute_import, unicode_literals, print_function
 from urllib3.exceptions import NewConnectionError, ConnectTimeoutError, MaxRetryError
 from kubernetes import client, config
 from kubernetes.client.api_client import ApiException
+from typing import Dict, List
 
+import json
 import os
 import logging
 import urllib3
@@ -15,6 +17,9 @@ kubeconfig_filepath = os.getenv("KUBECONFIG") or "~/.kube/config"
 
 
 class KubernetesClient(object):
+    _cache_ready: bool = False
+    _cache_dir: str = "~/.kube/kube_shell/"
+    _cache_file: str = "cache.json"
 
     def __init__(self):
         self.logger = logging.getLogger(__name__)
@@ -32,7 +37,15 @@ class KubernetesClient(object):
         self.batchV1Api = client.BatchV1Api()
         self.batchV2Api = client.BatchV2alpha1Api()
 
+        self._namespace = self._parse_namespace()
+        self._cached_resources: Dict[str, List[str]] = {}
+        self._init_cached_resources(self._namespace)
+
     def get_resource(self, resource, namespace="all"):
+        if namespace != "all":
+            resources = self._get_cached_resource(resource)
+            return resources
+
         ret, resources = None, list()
         try:
             ret, namespaced_resource = self._call_api_client(resource)
@@ -47,6 +60,10 @@ class KubernetesClient(object):
                     resources.append((i.metadata.name, i.metadata.namespace))
                 elif namespace == i.metadata.namespace:
                     resources.append((i.metadata.name, i.metadata.namespace))
+        return resources
+
+    def _get_cached_resource(self, resource):
+        resources = self._cached_resources.get(resource) or []
         return resources
 
     def _call_api_client(self, resource):
@@ -120,3 +137,54 @@ class KubernetesClient(object):
         else:
             return None, namespaced_resource
         return ret, namespaced_resource
+
+    def _call_api_client_namespaced(self, resource: str, namespace: str) -> List[str]:
+        if resource == "pod":
+            resource_v1_list = self.v1.list_namespaced_pod(namespace=namespace)
+        elif resource == "service":
+            resource_v1_list = self.v1.list_namespaced_service(namespace=namespace)
+        elif resource == "secret":
+            resource_v1_list = self.v1.list_namespaced_secret(namespace=namespace)
+        elif resource == "configmap":
+            resource_v1_list = self.v1.list_namespaced_config_map(namespace=namespace)
+        else:
+            resource_v1_list = []
+
+        names = [item.metadata.name for item in resource_v1_list.items]
+        return names
+
+    def _init_cached_resources(self, namespace: str):
+        cache_dir_expanded = os.path.expanduser(KubernetesClient._cache_dir)
+        if not os.path.exists(cache_dir_expanded):
+            os.makedirs(cache_dir_expanded)
+        cache_file = cache_dir_expanded + KubernetesClient._cache_file
+
+        if not KubernetesClient._cache_ready:
+            print("[INFO] Caching resources.")
+            resource_names = ["service", "pod", "secret", "configmap"]
+            for resource_name in resource_names:
+                try:
+                    self._cached_resources[resource_name] = self._call_api_client_namespaced(resource_name, namespace)
+                except:
+                    self._cached_resources[resource_name] = []
+                    print(f"[ERROR] Getting namespaced resource {resource_name}")
+                    import sys
+                    exc_info = sys.exc_info()
+                    print(exc_info)
+
+            deployments = [self._pod_name_to_deploy_name(pod_name)
+                           for pod_name in self._cached_resources["pod"]]
+            deployments = list(set(deployments))
+            self._cached_resources["deployment"] = deployments
+
+            json.dump(self._cached_resources, open(cache_file, "w"))
+            KubernetesClient._cache_ready = True
+        else:
+            self._cached_resources = json.load(open(cache_file, "r"))
+
+    def _pod_name_to_deploy_name(self, pod_name: str) -> str:
+        deploy_name = "-".join(pod_name.split("-")[:-2])
+        return deploy_name
+
+    def _parse_namespace(self):
+        return "conversation"  # TODO: implement properly
